b0ba01d808f00b0048a458a1077c9573c5f73ab7
SachinSharma22 <s830sharma@gmail.com>
2025-07-23 09:19:03 +0530

    inbuilt middleware

---
+++ index5.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import fs from 'fs';
+import path from 'path';
+const app = express();
+const cssPath = path.resolve('public');
+app.use(express.static(cssPath));
+
+const absPath = path.resolve("view")
+app.get("/", (req, res) => {
+  res.sendFile(absPath+"/home.html");
+});
+
+app.use(express.urlencoded({ extended: true }));
+app.get("/login", (req, res) => {
+  fs.readFile("view/login.html", (err, data) => {
+    if (err) {
+      res.status(500).send("Error reading file");
+    } else {
+      res.send(data.toString());
+    }
+  });
+});
+
+app.post("/submit", (req, res) => {
+    console.log("user login successful")
+  res.send(req.body);
+});
+app.listen(3000, () => {
+  console.log("Server is running on http://localhost:3000");
+});
