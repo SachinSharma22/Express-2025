819f8dc2395e6320028b7fd3dd01f30353785011
SachinSharma22 <s830sharma@gmail.com>
2026-04-22 11:32:46 +0530

    learn route organization and route level middleware Integration

---
+++ workingOfExpress/controllers/userController.js
@@ -0,0 +1,11 @@
+export const getUser = (req,res) => {
+    res.send("All Users data from controller");
+};
+
+export const createUser = (req,res) => {
+    res.send("Controller created the user");
+};
+
+export const profile = (req,res) => {
+    res.send("Hi dear");
+}
--- workingOfExpress/routes/userRoutes.js
+++ workingOfExpress/routes/userRoutes.js
@@ -1,12 +1,20 @@
 import express from "express";
+import { getUser, createUser, profile } from "../controllers/userController.js";
+import { auth } from "../middleware/auth.js";
 const router = express.Router();
 
-router.get("/getUser", (req,res) => {
-    res.send("All users");
-});
 
-router.post('/createUser',(req,res) => {
-    res.send("Create user");
-});
+router.get("/getUser", getUser);
+router.post("/createUser", createUser);
+//route level middleware Integration
+router.get("/profile",auth,profile)
+
+// router.get("/getUser", (req,res) => {
+//     res.send("All users");
+// });
+
+// router.post('/createUser',(req,res) => {
+//     res.send("Create user");
+// });
 
 export default router;
